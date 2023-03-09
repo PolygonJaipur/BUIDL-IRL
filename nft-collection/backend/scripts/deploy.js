f8d054f97ab8090d0660e4772ed5ee0b87253f30
Yash Garg <yashygkf@gmail.com>
2023-03-10 02:11:49 +0530

    add: nft-collection backend

---
+++ nft-collection/backend/scripts/deploy.js
@@ -0,0 +1,38 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    try {
+        const NFT = await ethers.getContractFactory("BuidlNFT");
+        const nft = await NFT.deploy("BuidlNFT", "BN");
+        await nft.deployed();
+        console.log("Contract address:", nft.address);
+    } catch (error) {
+        console.error(error);
+    }
+
+    console.log("Sleeping.....");
+    await sleep(40000);
+
+    try {
+        await hre.run("verify:verify", {
+            address: nft.address,
+            constructorArguments: ["BuidlNFT", "BN"],
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
