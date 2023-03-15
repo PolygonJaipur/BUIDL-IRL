372d44b1332769f887b80203a605a7bec49fbe68
Yash Garg <yashygkf@gmail.com>
2023-03-15 15:08:40 +0530

    edit: Added enumerability to the NFT contract

--- staking-dapp/backend/contracts/BuidlNFT.sol
+++ staking-dapp/backend/contracts/BuidlNFT.sol
@@ -3,9 +3,10 @@ pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BuidlNFT is ERC721, ERC721URIStorage {
+contract BuidlNFT is ERC721, ERC721URIStorage, ERC721Enumerable {
     using Counters for Counters.Counter;
 
     /// @dev Counter to keep track of the token Id
@@ -52,6 +53,15 @@ contract BuidlNFT is ERC721, ERC721URIStorage {
 
     // The following functions are overrides required by Solidity.
 
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
     function _burn(
         uint256 tokenId
     ) internal override(ERC721, ERC721URIStorage) {
@@ -62,5 +72,11 @@ contract BuidlNFT is ERC721, ERC721URIStorage {
         uint256 tokenId
     ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
