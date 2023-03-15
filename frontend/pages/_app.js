8265064759167695fc5462e98cfe8ce1f8d3535e
Pratham <prathamsinghal2003@gmail.com>
2023-03-15 23:17:33 +0530

    Initial Commit

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import {useEffect,useState} from "react";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { polygonMumbai } from "wagmi/chains";
@@ -7,5 +8,18 @@ import { publicProvider } from "wagmi/providers/public";
 import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+  return (
+    <>
+      {ready ? (
+            <Component {...pageProps} />
+      ) : (
+        <div className="flex justify-center text-2xl my-10">Loading</div>
+      )}
+    </>
+  );
 }
