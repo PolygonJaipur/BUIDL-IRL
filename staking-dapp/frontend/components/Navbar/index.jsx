c5f5ef0997b190f217049bf6219df23057ea19da
Pratham <prathamsinghal2003@gmail.com>
2023-03-16 10:19:46 +0530

    :rocket: Changed wallet

--- staking-dapp/frontend/components/Navbar/index.jsx
+++ staking-dapp/frontend/components/Navbar/index.jsx
@@ -1,21 +1,11 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Web3Button } from "@web3modal/react";
 import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between m-5">
       <h2 className="text-2xl font-medium">Buidl Staking </h2>
-      <ConnectButton
-        chainStatus="icon"
-        accountStatus={{
-          smallScreen: "avatar",
-          largeScreen: "full",
-        }}
-        showBalance={{
-          smallScreen: false,
-          largeScreen: true,
-        }}
-      />
+      <Web3Button />
     </nav>
   );
 };
--- staking-dapp/frontend/pages/_app.js
+++ staking-dapp/frontend/pages/_app.js
@@ -1,27 +1,26 @@
 import "@/styles/globals.css";
 import { useEffect, useState } from "react";
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { polygon, polygonMumbai } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
 import Navbar from "@/components/Navbar";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
 
-const { chains, provider } = configureChains(
-  [polygon, polygonMumbai],
-  // [alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY! }), publicProvider()]
-  [publicProvider()]
-);
-const { connectors } = getDefaultWallets({
-  appName: "My Staking Dpp",
-  chains,
-});
+const chains = [polygonMumbai];
+const projectId = process.env.NEXT_PUBLIC_WEB3_PROJECT_ID;
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiClient = createClient({
   autoConnect: true,
-  connectors,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
   provider,
 });
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
 export default function App({ Component, pageProps }) {
   const [ready, setReady] = useState(false);
 
@@ -32,10 +31,9 @@ export default function App({ Component, pageProps }) {
     <>
       {ready ? (
         <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider chains={chains} modalSize="compact">
-            <Navbar />
-            <Component {...pageProps} />
-          </RainbowKitProvider>
+          <Navbar />
+          <Component {...pageProps} />
+          <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
         </WagmiConfig>
       ) : (
         <div className="flex justify-center text-2xl my-[50%]">Loading</div>
