10b4597cb295bac9210e0d6be3a5e68bc4552f1e
Yash Garg <yashygkf@gmail.com>
2023-03-25 11:13:14 +0530

    completed: Mint Function Gasless

--- gasless-transaction/frontend/pages/mint.jsx
+++ gasless-transaction/frontend/pages/mint.jsx
@@ -1,26 +1,29 @@
-import { NextPage } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 import NFTAbi from "@/ABIs/BuidlNFT.json";
-import { useAccount, useSigner, useContract } from "wagmi";
 
-const Mint = () => {
+const Mint = ({ smartAccount }) => {
   const router = useRouter();
-  const { data: signer } = useSigner();
-  const { address } = useAccount();
-  const nftContract = useContract({
-    address: NFTAbi.address,
-    abi: NFTAbi.abi,
-    signerOrProvider: signer,
-  });
 
   const mintNFT = async () => {
     try {
-      const tx = await nftContract?.safeMint(
-        address,
-        "https://bafkreih73g4bdfee55w7izme3ryt6imjuh2nykdnxxpwe6eepdqrrkjcjm.ipfs.nftstorage.link/"
-      );
-      console.log(tx);
+      const nftContract = new ethers.utils.Interface(NFTAbi.abi);
+      const data = nftContract.encodeFunctionData("safeMint", [
+        smartAccount.address,
+        "https://bafkreih73g4bdfee55w7izme3ryt6imjuh2nykdnxxpwe6eepdqrrkjcjm.ipfs.nftstorage.link/",
+      ]);
+      const tx = {
+        to: NFTAbi.address,
+        data: data,
+      };
+
+      const txResponse = await smartAccount.sendGasLessTransaction({
+        transaction: tx,
+      });
+      console.log("tx hash generated", txResponse.hash);
+      const receipt = await txResponse.wait();
+      console.log("tx receipt", receipt);
+
       window.alert("NFT Minted");
       router.push("/");
     } catch (err) {
@@ -29,7 +32,7 @@ const Mint = () => {
   };
   return (
     <div>
-      {address && (
+      {smartAccount.address && (
         <button
           className="p-3 bg-[#ff1] text-black text-lg rounded-xl mx-[50%] min-w-[150px] my-5 hover:scale-105 font-medium"
           onClick={mintNFT}
